2abf9602a7df6ee9d902aa0c3db39e5c9c7bbd13
ojohnsons <ojohnson@tcd.ie>
2026-02-26 18:49:58 +0000

    For summarising Crab files on REALTA.

--- observation/pull-crab.py
+++ observation/pull-crab.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def main():
-    obs_df = pd.read_csv('./file-list/REALTA-Observation-Files.csv')
+    obs_df = pd.read_csv('/mnt/ucc4_data2/data/Owen/software/REALTA-Processing/csv_files/REALTA-Observation-Files.csv')
     print(obs_df.keys())
     print('Total observations: {}'.format(len(obs_df)))
     
@@ -12,7 +12,7 @@ def main():
     if any(source_names.str.contains(str1)) or any(source_names.str.contains(str2)) or any(source_names.str.contains(str3)):
         crab_df = obs_df[source_names.str.contains(str1) | source_names.str.contains(str2) | source_names.str.contains(str3)]
         # print(crab_df)
-        crab_df.to_csv('./file-list/REALTA-Crab-Files.csv', index=False)
+        crab_df.to_csv('../REALTA-Crab-Files.csv', index=False)
         
     print('Crab observations span from {} to {}'.format(
         crab_df['time_mjd'].min(), crab_df['time_mjd'].max()
